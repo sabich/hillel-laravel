768dc01220b189f10048d45877f9859d1211f3c8
Sergey Borisov <borisov_s@ukr.net>
2021-05-08 07:50:46 +0300

    HW-11: json to xml

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\InvoiceController;
+use App\Http\Controllers\Api\JsonToXmlController;
 use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\PartnerController;
 use App\Http\Controllers\Api\ProductController;
@@ -41,4 +42,5 @@ Route::group(['namespace' => 'Api'], function () {
         Route::get('/', [InvoiceController::class, 'index']);
         Route::get('/paid', [InvoiceController::class, 'paid']);
     });
+    Route::get('json-to-xml', [JsonToXmlController::class, 'index']);
 });
